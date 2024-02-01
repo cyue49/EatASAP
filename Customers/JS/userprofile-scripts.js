9f18c4843836e846f58af4f66ea7508ace3ce01f
Chen Yue <yuechen049@gmail.com>
2024-02-01 16:36:04 -0500

    added opening and closing of popup windows for user profile page

--- Customers/JS/userprofile-scripts.js
+++ Customers/JS/userprofile-scripts.js
@@ -1,10 +1,27 @@
 // Toggle between up and down arrows for order history collapsible
-$(".fa-angle-down").on('click', function(event){
+$(".fa-angle-down").on('click', function (event) {
     $(this).next().css("display", "block");
     $(this).css("display", "none");
 });
 
-$(".fa-angle-up").on('click', function(event){
+$(".fa-angle-up").on('click', function (event) {
     $(this).prev().css("display", "block");
     $(this).css("display", "none");
+});
+
+// Edit user info & payment info popups
+$('#editUserInfoPopupButton').click(function () {
+    $('#userInfoEditPopup').css("display", "flex");
+});
+
+$('#editPaymentMethodPopupButton').click(function () {
+    $('#paymentInfoEditPopup').css("display", "flex");
+});
+
+$('#closeUserInfoPopup').click(function () {
+    $('#userInfoEditPopup').css("display", "none");
+});
+
+$('#closePaymentInfoPopup').click(function () {
+    $('#paymentInfoEditPopup').css("display", "none");
 });
