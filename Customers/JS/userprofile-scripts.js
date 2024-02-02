a8d5dc16b1fa8d6f7e704bad2f8bdc4e33a1aa8e
Chen Yue <yuechen049@gmail.com>
2024-02-01 20:48:16 -0500

    added form for popup windows for user profile page

--- Customers/JS/userprofile-scripts.js
+++ Customers/JS/userprofile-scripts.js
@@ -10,6 +10,7 @@ $(".fa-angle-up").on('click', function (event) {
 });
 
 // Edit user info & payment info popups
+// open popup windows with 'edit' icon
 $('#editUserInfoPopupButton').click(function () {
     $('#userInfoEditPopup').css("display", "flex");
 });
@@ -18,10 +19,29 @@ $('#editPaymentMethodPopupButton').click(function () {
     $('#paymentInfoEditPopup').css("display", "flex");
 });
 
+// close popu up windows with 'X' icon
 $('#closeUserInfoPopup').click(function () {
     $('#userInfoEditPopup').css("display", "none");
 });
 
 $('#closePaymentInfoPopup').click(function () {
+    $('#paymentInfoEditPopup').css("display", "none");
+});
+
+// submit button
+$('#editProfileInfoDone').click(function () {
+    $('#userInfoEditPopup').css("display", "none");
+});
+
+$('#editPaymentInfoDone').click(function () {
+    $('#paymentInfoEditPopup').css("display", "none");
+});
+
+// cancel button
+$('#editProfileInfoCancel').click(function () {
+    $('#userInfoEditPopup').css("display", "none");
+});
+
+$('#editPaymentInfoCancel').click(function () {
     $('#paymentInfoEditPopup').css("display", "none");
 });
