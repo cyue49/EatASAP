d32b0745b2f59ff2ae20f187a29c460d54d6a559
Chen Yue <yuechen049@gmail.com>
2024-02-02 21:12:17 -0500

    added footer and updated customer profile page to apply changes when user edit profile information

--- Customers/JS/userprofile-scripts.js
+++ Customers/JS/userprofile-scripts.js
@@ -30,10 +30,33 @@ $('#closePaymentInfoPopup').click(function () {
 
 // submit button
 $('#editProfileInfoDone').click(function () {
+    // if user edited name
+    if ($('#firstName').val() != "" && $('#lastName').val() != ""){
+        sessionStorage.setItem("userFirstName", $('#firstName').val());
+        sessionStorage.setItem("userLastName", $('#lastName').val());
+        $('#userFullName').html(sessionStorage.getItem("userFirstName") + " " + sessionStorage.getItem("userLastName"));
+    }
+    // if user edited email
+    if ($('#emailAddress').val() != ""){
+        sessionStorage.setItem("userEmailAddress", $('#emailAddress').val());
+        $('#userEmailAddress').html(sessionStorage.getItem("userEmailAddress"));
+    }
+    // if user edited phone number
+    if ($('#phoneNumber').val() != ""){
+        sessionStorage.setItem("userPhoneNum", $('#phoneNumber').val());
+        $('#userPhoneNum').html(sessionStorage.getItem("userPhoneNum"));
+    }
+    // close user information edit popup
     $('#userInfoEditPopup').css("display", "none");
 });
 
 $('#editPaymentInfoDone').click(function () {
+    // if user edited card number
+    if ($('#cardNumber').val() != ""){
+        sessionStorage.setItem("userCardNum", $('#cardNumber').val());
+        $('#userCardNum').html("************" + sessionStorage.getItem("userCardNum").substr(12,15));
+    }
+    // closer payment information edit popup
     $('#paymentInfoEditPopup').css("display", "none");
 });
 
@@ -44,4 +67,33 @@ $('#editProfileInfoCancel').click(function () {
 
 $('#editPaymentInfoCancel').click(function () {
     $('#paymentInfoEditPopup').css("display", "none");
+});
+
+// User Information Displayed 
+$( document ).ready(function() {
+    // get variables from webstorage
+    var userFirstName = sessionStorage.getItem("userFirstName");
+    var userLastName = sessionStorage.getItem("userLastName");
+    var userEmailAddress = sessionStorage.getItem("userEmailAddress");
+    var userPhoneNum = sessionStorage.getItem("userPhoneNum");
+    var userCardNum = sessionStorage.getItem("userCardNum");
+
+    if (userFirstName == null || userLastName == null){
+        userFirstName = "Firstname";
+        userLastName = "Lastname";
+    }
+    if (userEmailAddress == null){
+        userEmailAddress = "No email address provided";
+    }
+    if (userPhoneNum == null){
+        userPhoneNum = "No phone number provided";
+    }
+    if (userCardNum == null){
+        userCardNum = "none";
+    }
+
+    $('#userFullName').html(userFirstName + " " + userLastName);
+    $('#userEmailAddress').html(userEmailAddress);
+    $('#userPhoneNum').html(userPhoneNum);
+    $('#userCardNum').html("************" + userCardNum.substr(12,15));
 });
