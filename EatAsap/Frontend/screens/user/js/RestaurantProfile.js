23b5d5f330e9a3e55ee5ca22413e3f9da3c024bb
unknown <kawtharmashkour@yahoo.com>
2024-02-04 21:25:36 -0500

    add delete features done

--- EatAsap/Frontend/screens/user/js/RestaurantProfile.js
+++ EatAsap/Frontend/screens/user/js/RestaurantProfile.js
@@ -1,6 +1,6 @@
 const delBtns = document.getElementsByClassName("fa-trash-alt");
 const loginBtnTxt = document.getElementById("login").getElementsByTagName("p")[0];
-const realDelBtn = document.querySelectorAll("main .card button");
+
 const formSection = document.getElementById("form-section");
 const form = document.getElementById("form");
 const submitBtn = document.getElementById("submit-btn");
@@ -8,6 +8,19 @@ const log = document.getElementById("login");
 
 document.addEventListener('DOMContentLoaded', function () {
     addMenuItems();
+    //debugger;
+    //delete events
+    const allDelBtn = document.querySelectorAll("main .card .del-btn");
+    for (let btn of allDelBtn) {
+            btn.addEventListener("click", delCard);
+        }
+        
+    //edit events
+    const allEditBtn = document.querySelectorAll("main .card .edit-btn");
+    for (let btn of allEditBtn) {
+            btn.addEventListener("click", editCard);
+    }
+    
 });
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -24,12 +37,7 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
-for (let btn of realDelBtn) {
-    btn.addEventListener("click", delCard);
-}
-
-
-form.addEventListener("submit", addItem);
+form.addEventListener("submit", addCard);
 
 document.addEventListener('DOMContentLoaded', function () {
     var addButton = document.getElementById('add-btn');
@@ -90,7 +98,7 @@ function addMenuItems() {
             title: "Vegetarian Pad Thai",
             description: "Stir-fried rice noodles with tofu, bean sprouts, and peanuts.",
             price: "$11.95",
-            imageSrc: "../../assets/pictures/PlatesPictures/plate5.jpg",
+            imageSrc: "../../assets/pictures/PlatesPictures/plate4.jpg",
         },
         {
             title: "Classic Margherita Pizza",
@@ -127,38 +135,38 @@ function addMenuItems() {
 
 }
 
-function addItem() {
+// function addItem() {
+// //debugger;
+//     const title = document.getElementById("title").value;
+//     const price = document.getElementById("price").value;
+//     const description = document.querySelector("textarea").value;
+//     const imageSrc = "../../assets/pictures/PlatesPictures/plate1.jpg"
 
-    const title = document.getElementById("title").value;
-    const price = document.getElementById("price").value;
-    const description = document.querySelector("textarea").value;
-    const imageSrc = "../../assets/pictures/PlatesPictures/plate1.jpg"
 
+//     const listItem = document.createElement('li');
+//     listItem.classList.add('card');
 
-    const listItem = document.createElement('li');
-    listItem.classList.add('card');
+//     // Set the inner HTML for the li element
+//     listItem.innerHTML = `
+//         <img class="item-image" src="${imageSrc}" alt="Item Picture1">
+//         <div class="text">
+//             <h3 class="i-title">${title}</h3>
+//             <p class="i-description">${description}</p>
+//             <h3 class="i-price">${price}</h3>
+//         </div>
+//         <div class="buttons">
+//             <div class="edit-btn">
+//                 <i class='far fa-edit' style="font-size:24px !important;color: var(--secondary-color)"></i>
+//             </div>
+//             <div class="del-btn">
+//                 <i class="fas fa-trash-alt" style="font-size:24px"></i>
+//             </div>
+//         </div>`;
 
-    // Set the inner HTML for the li element
-    listItem.innerHTML = `
-        <img class="item-image" src="${item.imageSrc}" alt="Item Picture1">
-        <div class="text">
-            <h3 class="i-title">${title}</h3>
-            <p class="i-description">${description}</p>
-            <h3 class="i-price">${price}</h3>
-        </div>
-        <div class="buttons">
-            <div class="edit-btn">
-                <i class='far fa-edit' style='color: var(--secondary-color)'></i>
-            </div>
-            <div class="del-btn">
-                <i class="fas fa-trash-alt" style="font-size:24px"></i>
-            </div>
-        </div>`;
+//     const parentContainer = document.getElementById('newmenulist');
+//     parentContainer.appendChild(listItem);
 
-    const parentContainer = document.getElementById('newmenulist');
-    parentContainer.appendChild(listItem);
-
-}
+// }
 
 
 function login(isLogin) {
@@ -182,41 +190,81 @@ function delCard(event) {
     event.currentTarget.parentElement.parentElement.remove();
 }
 
-// function addCard(event) {
-//     event.preventDefault();
-//     // console.log(form.checkValidity()); // should be true
+function addCard(event) {
+    event.preventDefault();
 
-//     const title = document.getElementById("title").value;
-//     const price = document.getElementById("price").value;
-//     const description = document.querySelector("textarea").value;
+    const title = document.getElementById("title").value;
+    const price = document.getElementById("price").value;
+    const description = document.querySelector("textarea").value;
+    const imageSrc = "../../assets/pictures/comingSoon.jpg"
 
-//     const textDiv = createSimpleNode("div", "text");
-//     textDiv.appendChild(createNodeWithText("h3", title, "i-title"));
-//     textDiv.appendChild(createNodeWithText("p", description, "i-description"));
-//     textDiv.appendChild(createNodeWithText("h3", '$' + price, "i-price"));
 
-//     const buttonDiv = createSimpleNode("div", "buttons");
-//     const delButton = createSimpleNode("button", "btn");
-//     const buttonI = createSimpleNode("i", "fas fa-trash-alt");
+    const listItem = document.createElement('li');
+    listItem.classList.add('card');
 
-//     delButton.style.display = 'inline';  // by default .btn has display: none
+    // Set the inner HTML for the li element
+    listItem.innerHTML = `
+        <img class="item-image" src="${imageSrc}" alt="Item Picture1">
+        <div class="text">
+            <h3 class="i-title">${title}</h3>
+            <p class="i-description">${description}</p>
+            <h3 class="i-price">$${price}</h3>
+        </div>
+        <div class="buttons">
+            <div class="edit-btn">
+                <i class='far fa-edit' style='color: var(--secondary-color)'></i>
+            </div>
+            <div class="del-btn">
+                <i class="fas fa-trash-alt" style="font-size:24px"></i>
+            </div>
+        </div>`;
 
-//     delButton.appendChild(buttonI);
-//     buttonDiv.appendChild(delButton);
+    const parentContainer = document.getElementById('newmenulist');
+    parentContainer.appendChild(listItem);
 
-//     const li = createSimpleNode("li", "card");
-//     li.appendChild(textDiv);
-//     li.appendChild(buttonDiv);
-//     document.querySelector("ul").appendChild(li);
+    const realDelBtn = document.querySelectorAll("main .card .del-btn");
+    for (let btn of realDelBtn) {
+            btn.addEventListener("click", delCard);
+        }
+    //edit events
+    const allEditBtn = document.querySelectorAll("main .card .edit-btn");
+    for (let btn of allEditBtn) {
+            btn.addEventListener("click", editCard);
+    }
 
-//     // adding event listener to newly created button
-//     delButton.addEventListener("click", delCard);
+    // const textDiv = createSimpleNode("div", "text");
+    // textDiv.appendChild(createNodeWithText("h3", title, "i-title"));
+    // textDiv.appendChild(createNodeWithText("p", description, "i-description"));
+    // textDiv.appendChild(createNodeWithText("h3", '$' + price, "i-price"));
 
-//     // Clear input fields
-//     document.getElementById("title").value = '';
-//     document.getElementById("price").value = '';
-//     document.querySelector("textarea").value = '';
-// }
+    // const buttonDiv = createSimpleNode("div", "buttons");
+    // const delButton = createSimpleNode("button", "btn");
+    // const buttonI = createSimpleNode("i", "fas fa-trash-alt");
+
+    // delButton.style.display = 'inline';  // by default .btn has display: none
+
+    // delButton.appendChild(buttonI);
+    // buttonDiv.appendChild(delButton);
+
+    // const li = createSimpleNode("li", "card");
+    // li.appendChild(textDiv);
+    // li.appendChild(buttonDiv);
+    // document.querySelector("ul").appendChild(li);
+
+    // // adding event listener to newly created button
+    // delButton.addEventListener("click", delCard);
+
+    // Clear input fields
+    document.getElementById("title").value = '';
+    document.getElementById("price").value = '';
+    document.querySelector("textarea").value = '';
+}
+
+function editCard () {
+
+    window.location.href = '../../signin.html';
+
+}
 
 // Helper functions to avoid repetition.
 
