1ec6947d3502242d3a448491bbae6496378e50e0
Ashotbon <154383170+Ashotbon@users.noreply.github.com>
2024-02-23 09:59:13 -0500

    ...

--- EatAsap/Backend/eatasap.sql
+++ EatAsap/Backend/eatasap.sql
@@ -8,7 +8,16 @@ USE eatasap;
 
 /* ============== CREATING TABLES ============== */
 /*Contact Us*/
- 
+ CREATE TABLE contact_submissions (
+    submission_id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    phone VARCHAR(255),
+    message TEXT NOT NULL,
+   
+);
+
 
 /* User */
 
