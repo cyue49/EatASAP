e1a46eeb64ff0b57352a0f9079b17632ec34936f
unknown <kawtharmashkour@yahoo.com>
2024-02-26 20:40:22 -0500

    update config.php for production

--- EatAsap/Backend/DB/config.php
+++ EatAsap/Backend/DB/config.php
@@ -13,4 +13,18 @@ $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 if($link === false){
     die("ERROR: Could not connect DB. " .": ".mysqli_connect_error());
 }
+
+// production connection
+// $servername = "localhost";
+// $username = "id21930259_root";
+// $password = "Eatasap1@";
+// $dbname = "id21930259_eatasap";
+ 
+// // Create connection
+// $conn = new mysqli($servername, $username, $password, $dbname);
+ 
+// // Check connection
+// if ($conn->connect_error) {
+//     die("Connection failed: " . $conn->connect_error);
+// }
 ?>
