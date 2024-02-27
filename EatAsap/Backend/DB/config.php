b5d8fb8720bd623d39b7d669c84da320eae68000
unknown <kawtharmashkour@yahoo.com>
2024-02-27 06:51:21 -0500

    update config.php for production

--- EatAsap/Backend/DB/config.php
+++ EatAsap/Backend/DB/config.php
@@ -1,25 +1,11 @@
 <?php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'eatasap');
+// define('DB_SERVER', 'localhost');
+// define('DB_USERNAME', 'root');
+// define('DB_PASSWORD', '');
+// define('DB_NAME', 'eatasap');
  
-/* Attempt to connect to MySQL database */
-$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
- 
-// Check connection
-if($link === false){
-    die("ERROR: Could not connect DB. " .": ".mysqli_connect_error());
-}
-
-/// production connection
-// $servername = "localhost";
-// $username = "id21930259_root";
-// $password = "Eatasap1@";
-// $dbname = "id21930259_eatasap";
-
 // /* Attempt to connect to MySQL database */
 // $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
  
@@ -27,5 +13,19 @@ if($link === false){
 // if($link === false){
 //     die("ERROR: Could not connect DB. " .": ".mysqli_connect_error());
 // }
+
+// production connection
+$servername = "localhost";
+$username = "id21930259_root";
+$password = "Eatasap1@";
+$dbname = "id21930259_eatasap";
+
+/* Attempt to connect to MySQL database */
+$link = mysqli_connect($servername, $username, $password, $dbname);
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect DB. " .": ".mysqli_connect_error());
+}
 
 ?>
